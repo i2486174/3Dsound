82dff2fba97494e10a6e7d4d2e6b9ff999658b4e
i2486174 <166976635+i2486174@users.noreply.github.com>
2024-11-26 17:35:35 +0900

    3Dsound.mを修正

--- src/Matlab/3Dsound.m
+++ src/Matlab/3Dsound.m
@@ -1,93 +1,114 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% 音響MRを作る: 
-% 3点を利用してBRIRを検出する
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% BRIRを検出し，音響MRを作成する
 clear;
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% 立体音響化する音源の読み込み %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 [s, fs] = audioread("こんにちは.wav");  % 立体音響化する音源
-s = s./max(abs(s))*0.1; % 音量を下げる 
-[s0, fs2] = audioread("impulse_te_5.wav"); % 廊下
+s3d=s*1.0;
 
+[s, fs] = audioread("impulse_te_5.wav");   % 生活音
+buff = fs*0.5;
+Len = length(s);                        % 信号の長さ
+D   = 10;                               % 信号の変化量(傾き)を調べる範囲(片側)
+Th  = 0.4;                              % 傾きのしきい値 
+cnt = 0;                                % BRIR判別用カウンタ
+HFB_max = 0;                            % 傾きの最大値を初期化
+HFB_now = 0;                            % 現在の傾きを初期化
+num=0;                                  % BRIRの検出数
+x = zeros(buff,2);                      % 1秒間の入力ベクトル
+time_index=[];                          % BRIRの時刻記録
+BRIR=[];                                % BRIR波形記録
+y=s;                                    % 出力＝入力
+n=0;                                    % 3D Soundの時刻管理
+flg=0;
+Len3D=0;
+x_s  = zeros(buff,2);                   % xの2乗
+x_ss = 0;                               % x(Lch,Rch)の2乗和
+brir = zeros(buff,2);                   % 検出したxのエネルギー
+HBias = zeros(3,Len);                   % 検出したxのエネルギー
 
-%%%%%%%%%%%%%%%%%%%%%%%%%
-%  インパルス応答の検出 %
-%%%%%%%%%%%%%%%%%%%%%%%%%
-ImpLen = 0.5*fs;         % 最終的なインパルス応答の長さを0.4秒にする
-Imp=[];                  % インパルス応答の格納用変数
-posi = [];               % インパルスの場所を格納する変数 廊下
-N = length(s0);          % サンプル数
-for i=20:N               % 毎サンプルずつ調べる
-    x1=0; x2=0;
-    max1= abs(s0(i-10,1));    % 1chの最大振幅と位置を取得
-    max2= abs(s0(i-10,2));    % 2chの最大振幅と位置を取得
-    if max1 == max(max1, max2) % 音源が1ch寄りの時
-        x1 = s0(i-10,1)-s0(i-19,1); % 3点を約0.0002sの間隔でとる
-        x2 = s0(i,1)-s0(i-10,1); 
-    else % 2ch寄りのとき
-        x1 = s0(i-10,2)-s0(i-19,2); % 3点を約0.0002sの間隔でとる
-        x2 = s0(i,2)-s0(i-10,2); % 音源が長いときは，5sくらい
-    end
-    if x1 * x2  <= -0.4
-        pos = i -10; % 頂点のポジション
-        if ~isempty(posi) && abs(pos - posi(length(posi))) < 1.0 * fs 
-            % 1秒以内に同じ音量のインパルスを検出したら，
-            % その前のインパルスを消してインパルスではないようにする
-            continue
-        end
-        posi = [posi pos]; %ポジションを覚えておく
-        st = max([pos-fs/20, 1]);           % 少し手前をインパルス応答の開始位置とする
-        en = min(st+ImpLen, length(s0(:,1))); % 0.4秒間のインパルス応答を採用
-        yin=s0(st:en,:);                      % 抽出したインパルス応答
-        if(length(yin(:,1))<ImpLen)          % 長さが足りないときは0を追加する
-            L = ImpLen - length(yin(:,1))+1;
-            yin=[yin;zeros(L,2)];
-        end
-        yin=yin/norm(yin);             % 大きさを正規化する
-        Imp=[Imp yin];          % インパルス応答を格納
-    end
-end
-posNum = length(posi);
-x = input(posNum +" こ目のBRIRを検出");
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% 取得したインパルス応答の数だけ音源を分割する %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-K = length(posi);                             % K個のインパルス応答を取得する
-Len = length(s);                 % 1区間の長さ
-
-%%%%%%%%%%%%%%%%%%%%
-% 立体音響の作成   %
-%%%%%%%%%%%%%%%%%%%%
-out1=s0(:,1); out2=s0(:,2);                      % 2ch出力信号の初期化_廊下
-sz = size(out1);
-for i=1:K
-    %% 左
-    A = conv(s, Imp(:,2*i-1)); % 畳み込む_廊下
+for i=1:Len
+    x(1,:)   = s(i,:);                  % 現在の入力信号
     
-    b = zeros(sz); % 長さを合わせる
-    b(posi(1,i)+fs:posi(1,i)+fs+length(A)-1,:) = A; % 大きさをoutに揃える
-    %out1 = out1 + conv(DivS(:,i), Imp(:,2*i-1)); % 1ch output
-    if(length(out1)<length(b))          % 長さが足りないときは0を追加する
-        b1 = b;   
-        b = b1(1:length(out1),:);
+    % 現在の信号を含むbuff秒間をBRIRであると仮定する
+    HF1 = x(D,1) - min( x(2:D,1) );     % 前方の高さ変化1ch
+    HB1 = x(D,1) - min( x(D+1:2*D,1) ); % 後方の高さ変化1ch
+    HF2 = x(D,2) - min( x(2:D,2) );     % 前方の高さ変化2ch
+    HB2 = x(D,2) - min( x(D+1:2*D,2) ); % 後方の高さ変化2ch
+    HFB_now  = max(HF1*HB1, HF2*HB2);   % 現在の信号の傾き
+    HBias(1,i) = HFB_now;
+    HBias(2,i) = cnt;
+    HBias(3,i) = flg;
+    
+    % 信号がBRIRかどうかを判定する
+    if flg==0
+    if HFB_now>=Th                      % 傾きがしきい値を超えた？
+        cnt = 0;                        % カウンタをリセット
+        HFB_max = HFB_now;              % 傾きの最大値を更新
+    else if HFB_max==0                  % HFB_maxが初期値のまま？
+            cnt = 0;                    % カウンタをリセット
+         else                           % 現在の傾きがしきい値未満ならば
+            cnt = cnt+1;                % カウントアップ
+         end                             
     end
-    out1 = out1 + b; % 1ch output
-    %% 右
-    B = conv(s, Imp(:,2*i)); %畳み込む
-  
-    b = zeros(sz); % 長さを合わせる
-    b(posi(1,i)+fs:posi(1,i)+fs+length(B)-1,:) = B; 
-    if(length(out2)<length(b))          % 長さが足りないときは0を追加する
-        b2 = b;
-        b = b2(1:length(out2),:);
+    
+    % BRIRだった場合，検出した信号を利用して立体音響を作成
+    if (cnt >= buff-441)                  % カウンタ1秒弱経過？
+        x_s  = x.^2;
+        x_ss = sum(x_s,"all");
+        brir = x ./ x_ss .* 2;
+        Add_3D = [];
+        BRIR = [BRIR flip(brir)];           % 観測信号をBRIRとする
+        Add_L = conv(s3d, BRIR(:,num*2+1)); % 3D Sound Lch
+        Add_R = conv(s3d, BRIR(:,num*2+2)); % 3D Sound Rch
+        Add_3D = [Add_L, Add_R];            % 3D Stereo Sound
+        Add_3D = Add_3D/max(Add_3D(:))*0.5;
+        Len3D = length(Add_L);
+        %Len3D = fs;
+        num=num+1;                      % BRIRの検出数を＋1
+        time_index = [time_index; HFB_max, i]; % BRIRの終了時刻を記録
+        HFB_max=0;                      % 傾きの最大値を初期化
+        cnt  = 0;                       % カウンタをリセット
+        n=1;                            % 3D sound time index
+        flg=1;                          % 3D sound addition flag
     end
-    out2 = out2 + b;   % 2ch output
+    end
+    x(2:end,:)=x(1:end-1,:);            % 1秒間の入力ベクトルをシフト
+
+    if flg==1                           % flag=1のとき
+        y(i,:)=s(i,:)+Add_3D(n,:);      % 3D Sound Addition
+        n=n+1;                          % 3D Sound時刻＋1
+    end
+    if n>=Len3D                         % 3D音響付加が2秒を超えると初期状態に戻す
+        %flg=0;
+        n=0;
+    end
+    if n>=Len3D/2                        % 3D音響付加が2秒を超えると初期状態に戻す
+        flg=0;
+    end
 end
-out = [out1,out2];                 % 2ch立体音響
-out = out./max(abs(out))*0.8;      % 全体の振幅を調整
-disp('Start Writing.');            % 書き出し開始
-audiowrite('./rslt_mr6_hello/output.wav',out,fs); % 廊下で廊下のBRIR
-disp('End of Writing.');           % 書き出し終わり
-%disp("3D sound play");
-%sound(out,fs);                    % 立体音響を再生する
+
+%% 入力と出力をwavファイルに保存
+%Add_L2 = conv(s3d, BRIR(:,3)); % 3D Sound Lch
+%Add_R2 = conv(s3d, BRIR(:,4)); % 3D Sound Rch
+%Add_3D2 = [Add_L2, Add_R2];        % 3D Stereo Sound
+%Add_3D2 = Add_3D2/max(Add_3D2(:))*0.5;
+%BRIR_out = [BRIR(:,3), BRIR(:,4)];
+audiowrite('input.wav',  s, fs);
+audiowrite('output.wav', y, fs);     
+
+
+% 検出したBRIRをプロット
+figure(1);clf;
+for i=1:num*2
+    subplot(num, 2, i);
+    plot(BRIR(:,i));
+end
+figure(2);clf;
+subplot(5,1,1);
+plot(s);
+subplot(5,1,2);
+plot(HBias(1,:));
+subplot(5,1,3);
+plot(HBias(2,:));
+subplot(5,1,4);
+plot(HBias(3,:));
+subplot(5,1,5);
+plot(y);
